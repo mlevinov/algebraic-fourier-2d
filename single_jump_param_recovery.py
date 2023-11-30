edecb9e2b1bc46b749197c3ce6ba619af2098c90
michaellevinov <m.levinov@gmail.com>
2023-11-30 22:09:04 +0200

    working on single_jump_param_recovery - _polynomial_coefficients

--- single_jump_param_recovery.py
+++ single_jump_param_recovery.py
@@ -1,6 +1,8 @@
+import sys
 import mpmath as mpm
 import numpy as np
 import constants as const
+from mpmath import libmp
 
 def set_mpmath_precision(dps=20):
     mpm.mp.dps=dps
@@ -63,3 +65,44 @@ def __calc_coeff_phi(k, reconstruction_order, jump_loc, jump_mag_array):
         return mpm.fmul(r1, 2)
 def __calc_coeff_psi(func_coeff_at_k, phi_coeff_at_k):
     return mpm.fsub(func_coeff_at_k, phi_coeff_at_k)
+def poly_roots(polynomial_coefficients_array):
+    if polynomial_coefficients_array.cols != 1:
+        polynomial_coefficients_array = polynomial_coefficients_array.T
+    tries = 10
+    max_steps = 50
+    extra_prec = 10
+    polynomial_roots = []
+    convergenceFlag = True
+    while tries > 0:
+        try:
+            polynomial_roots = mpm.polyroots(polynomial_coefficients_array, maxsteps=max_steps,
+                                             extraprec=extra_prec)
+            convergenceFlag = True
+            break
+        except mpm.libmp.libhyper.NoConvergence:
+            convergenceFlag = False
+            max_steps += 25
+            extra_prec += 10
+            tries -= 1
+        except ZeroDivisionError:
+            print('\nZeroDivisionError:')
+            print('\ncoefficients for polynomial are:\n{}'.format(polynomial_coefficients_array))
+            sys.exit('\nfirst coefficient must be NOT ZERO\n')
+    if not convergenceFlag:
+        # raise NoRootConvergenceError(tries, max_steps, extra_prec)
+        print('')
+    return polynomial_roots
+def _polynomial_coefficients(reconstruction_order, func_coeff_array, half_order_flag=False):
+    if func_coeff_array.cols != 1:
+        func_coeff_array = func_coeff_array.T
+
+    m = func_coeff_array.cols // 2
+
+    coefficients = mpm.matrix(reconstruction_order + 1, 1)
+    if half_order_flag:
+        for i in range(reconstruction_order + 2):
+            mk_tilde_val = const.TWO_PI
+            a = mpm.power(-1, i)
+            b = mpm.binomial(reconstruction_order + 1, i)
+            c = mpm.fmul(mpm.fmul(a, b), mk_tilde_val)
+            coefficients[i, 0] = c
