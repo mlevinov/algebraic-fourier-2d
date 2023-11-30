995b71f766ca6474e1157d7e7c6b5df3b791bcf1
mlevinov <m.levinov@gmail.com>
2023-11-30 10:45:38 +0200

    working on mpmath_to_numpy

--- mpmath_to_numpy.py
+++ mpmath_to_numpy.py
@@ -3,7 +3,38 @@ import mpmath as mpm
 import constants as const
 
 
-def mpmath_array_to_numpy_array(mp_arr):
-    if mp_arr.cols == 1 and mp_arr.rows == 1:
+def convert_mpm_to_mpmath_numpy(mpmath_num):
+    if isinstance(mpmath_num, mpm.mpf):
+        s = float(mpm.nstr(mpmath_num, mpm.mp.dps))
+        return s
+    elif isinstance(mpmath_num, mpm.mpc):
+        re = mpm.nstr(mpm.re(mpmath_num), mpm.mp.dps)
+        im = mpm.nstr(mpm.im(mpmath_num), mpm.mp.dps)
+        s = complex(float(re), float(im))
+        return s
+    elif isinstance(mpmath_num, float) or isinstance(mpmath_num, complex) or isinstance(mpmath_num, int):
+        return mpmath_num
+    else:
+        print('not an mpmath number type')
+        return 1
+def mpm_array_to_mpmath_numpy_array(mpmath_arr):
+    if isinstance(mpmath_arr, np.ndarray):
+        return mpmath_arr
+    elif isinstance(mpmath_arr, mpm.matrix):
+        print('not an mpmath matrix and not a numpy array type')
+        return 1
+    else:
+        mat_type = _check_type_of_mpmath_matrix(mpmath_arr)
+        return np.array(mpmath_arr.tolist(), dtype=mat_type)
 
-    if (mp_arr.cols == 1 and mp_arr.rows >1) or (mp_arr.cols > 1 and mp_arr.rows == 1):
+def _check_type_of_mpmath_matrix(mpmath_mat):
+    if not isinstance(mpmath_mat, mpm.matrix):
+        print('not an mpmath matrix')
+        return 1
+    cols = mpmath_mat.cols
+    rows = mpmath_mat.rows
+    for r in range(rows):
+        for c in range(cols):
+            if isinstance(mpmath_mat[r, c], mpm.mpc):
+                return complex
+    return float
