4499fe002299e514e54cf9567003fe59562e4337
michaellevinov <m.levinov@gmail.com>
2024-04-26 19:53:30 +0300

    added finished documenting mpmath_tools.py

--- mpmath_tools.py
+++ mpmath_tools.py
@@ -1,13 +1,32 @@
-import numpy as np
 import mpmath as mpm
-import constants as const
+import numpy as np
 
 
 def numpy_array_to_mpmath_matrix(np_arr):
+    """
+    converting from numpy ndarray to mpmath.mtrix type
+
+    Args:
+        np_arr: (ndarray) numpy ndarry to convert to mpmath.matrix
+
+    Returns:
+        mpmath.matrix: a matrix of type mpmath.matrix from np_arr
+
+    """
     return mpm.matrix(np_arr.tolist())
 
 
 def mpm_matrix_to_mpmath_numpy_array(mpmath_arr):
+    """
+    Converting a mpmath matrix to a ndarry
+
+    Args:
+        mpmath_arr: (mpmath.matrix) a matrix of type mpmath.matrix to convert
+
+    Returns:
+        ndarray: returns a ndarray from the given mpmath.matrix
+
+    """
     if isinstance(mpmath_arr, np.ndarray):
         return mpmath_arr
     # TODO: understand how to check if the input is of type mpmath.matrix
@@ -20,20 +39,48 @@ def mpm_matrix_to_mpmath_numpy_array(mpmath_arr):
 
 
 def find_max_val_index(mpmath_arr):
+    """
+    finding the index of the maximum value in a given matrix
+
+    Args:
+        mpmath_arr: (mpmath.matrix) a matrix containing the maximal value
+
+    Returns:
+        tuple: row and column indexes for the maximal value in mpmath_arr
+
+    """
     np_arr = mpm_matrix_to_mpmath_numpy_array(mpmath_arr)
     ind = np.unravel_index(np.argmax(np_arr, axis=None), np_arr.shape)
-    # TODO: returning a tuple of max_val_index, consider returning separately ind[0], ind[1]
     return ind
 
 
 def find_min_val_index(mpmath_arr):
+    """
+        finding the index of the minimal value in a given matrix
+
+        Args:
+            mpmath_arr: (mpmath.matrix) a matrix containing the minimal value
+
+        Returns:
+            tuple: row and column indexes for the minimal value in mpmath_arr
+
+        """
     np_arr = mpm_matrix_to_mpmath_numpy_array(mpmath_arr)
     ind = np.unravel_index(np.argmin(np_arr, axis=None), np_arr.shape)
-    # TODO: returning a tuple of max_val_index, consider returning separately ind[0], ind[1]
     return ind
 
 
 def mpmath_num_to_numpy(mpmath_num):
+    """
+    Converting a mpmath type value to a numpy type value
+
+    Args:
+        mpmath_num: (mpmath type) a value represented by the mpmath library
+
+    Returns:
+        numpy: same value as mpmath_num but using numpy library
+
+    """
     if isinstance(mpmath_num, mpm.mpf):
         s = float(mpm.nstr(mpmath_num, mpm.mp.dps))
         return s
@@ -63,6 +110,19 @@ def _check_type_of_mpmath_matrix(mpmath_mat):
 
 
 def elementwise_norm_matrix(mp_arr1, mp_arr2):
+    """
+    Calculates :math:`\\lvert \\text{mp_arr1[i,j]-mp_arr2[i,j]} \\rvert` for each :math:`1\\leq i\\leq m`
+    and :math:`1\\leq j\\leq n` where mp_arr1, mp_arr2 are of order :math:`m\\times n`
+
+    Args:
+        mp_arr1: (mpmath.matrix)
+        mp_arr2: (mpmath.matrix)
+
+    Returns:
+        mpmath.matrix: a matrix where each element is the absolut value of the difference between mp_arr1[i,j]
+        and mp_arr2[i,j]
+
+    """
     try:
         try:
             norm_mat = mp_arr1 - mp_arr2
